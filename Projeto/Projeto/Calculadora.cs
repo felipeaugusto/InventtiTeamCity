a9575e8bcd3e04d71e81097e6f2058097fb4c254
felipeaugusto <felipe@inventti.com.br>
2010-12-20 14:57:14 -0200

    alterado metodo Subtracao para nao ser estatico

--- Projeto/Projeto/Calculadora.cs
+++ Projeto/Projeto/Calculadora.cs
@@ -7,7 +7,7 @@
             return numero1 + numero2;
         }
 
-        public static float Subtracao(float numero1, float numero2)
+        public float Subtracao(float numero1, float numero2)
         {
             return numero1 - numero2;
         }
